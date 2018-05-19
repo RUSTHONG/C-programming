f541ac753af1f4f92e6a850edc536b6d07941680
RUSTHONG <1456774060@qq.com>
2018-05-19 12:12:41 +0800

    default

---
+++ 3.8_typesize/3.8_typesize.c
@@ -0,0 +1,13 @@
+//*typesize.c --打印类型大小*/
+#include <stdio.h>
+int main(void)
+{
+/*C99为类型大小提供%zd转换说明,sizeof是C语言的内置运算符,以字节为单位给出指定类型的大小。C99和C11提供%zd转换说明匹配sizeof的返回类型*/
+	printf("Type int has a size of %zd bytes.\n", sizeof(int));
+	printf("Type char has a size of %zd bytes.\n", sizeof(char));
+	printf("Type long has a size of %zd bytes.\n", sizeof(long));
+	printf("Type long long has a size of %zd bytes.\n", sizeof(long long));
+	printf("Type double has a size of %zd bytes.\n", sizeof(double));
+	printf("Type long double has a size of %zd bytes.\n", sizeof(long double));
+	return 0;
+}
