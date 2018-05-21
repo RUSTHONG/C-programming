b46cbbe80446578877908d85de643b518c03b66f
RUSTHONG <1456774060@qq.com>
2018-05-21 19:45:32 +0800

    default

---
+++ 4.17_skip2/4.17_skip2.c
@@ -0,0 +1,11 @@
+/*skiptwo.c -- 跳过输入中的前两个整数*/
+#include <stdio.h>
+int main(void)
+{
+	int n;
+	printf("Please enter three integers:\n");
+	scanf("%*d%*d%d", &n);
+	// scanf()中*的用法与此不同。把*放在%和转换字符之间时，会使得scanf()跳过相应的输出项
+	printf("The last integer was %d\n", n);
+	return 0;
+}
