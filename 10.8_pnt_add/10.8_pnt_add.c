2709a28ef4be4c1acc5fbd8f9a0ad531bf1e6d4d
RUSTHONG <1456774060@qq.com>
2018-06-01 20:25:52 +0800

    default

---
+++ 10.8_pnt_add/10.8_pnt_add.c
@@ -0,0 +1,13 @@
+// pnt_add.c -- 指针地址
+#include <stdio.h>
+#define SIZE 4
+int main(void)
+{
+	short dates[SIZE];
+	short *pti;
+	short index;
+	double bills[SIZE];
+	double *ptf;
+	pti = dates; // 把数组地址赋给指针
+	return 0;
+}
